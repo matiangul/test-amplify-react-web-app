53975c6a0427f803d13e8262225e8c4054765465
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2019-08-28 22:24:00 +0200

    Integrate with AWS app sync by using amplify api resourse

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listTodos = `
+query {
+  listTodos {
+    items {
+      id name description completed
+    }
+  }
+}`;
+
+class App extends React.Component {
+    state = { todos: [] };
+
+    async componentDidMount() {
+      const todos = await API.graphql(graphqlOperation(listTodos));
+      this.setState({ todos: todos.data.listTodos.items })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                {
+                  this.state.todos.map((todo, i) => (
+                    <div>
+                      <h3>{todo.name}</h3>
+                      <p>{todo.description}</p>
+                    </div>
+                  ))
+                }
+            </div>
+        );
+    }
 }
 
 export default withAuthenticator(App, { includeGreetings: true });
